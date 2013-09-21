0903bc20a184c018df5fa86182245fbf58fa7bdb
Lukáš Petrovický <lpetrovi@redhat.com>
2013-09-21 20:58:31 +0200

    Performance tweaks in partitioning strategy.

--- src/main/java/net/petrovicky/quicksort/impl/PartitioningStrategy.java
+++ src/main/java/net/petrovicky/quicksort/impl/PartitioningStrategy.java
@@ -1,12 +1,38 @@
 package net.petrovicky.quicksort.impl;
 
 /**
- * This class implements Quicksort's partitioning, giving users their choice of a pivot findning strategy.
+ * This class implements Quicksort's partitioning, giving users their choice of a pivot finding strategy.
+ * 
+ * Due to its nature, this class takes roughly 80 % of the whole app's run time. For this reason, even the tiniest
+ * performance improvements help the app a lot. This is why {@link #partition(Comparable[], int, int, int)} and
+ * {@link #swap(Object[], int, int)} are private static final, although they normally wouldn't be.
  * 
  * @param <T>
- *            Type of the list which we're partitioning.
+ *            Type of the data which we're partitioning.
  */
 public abstract class PartitioningStrategy<T extends Comparable<T>> {
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static final int partition(final Comparable[] input, final int left, final int right, final int pivotIndex) {
+        final Comparable pivotValue = input[pivotIndex];
+        PartitioningStrategy.swap(input, pivotIndex, right);
+        int storeIndex = left;
+        for (int i = left; i < right; i++) {
+            if (input[i].compareTo(pivotValue) >= 0) {
+                continue;
+            }
+            PartitioningStrategy.swap(input, i, storeIndex);
+            storeIndex++;
+        }
+        PartitioningStrategy.swap(input, storeIndex, right);
+        return storeIndex;
+    }
+
+    private static final void swap(final Object[] input, final int index1, final int index2) {
+        final Object tmp = input[index1];
+        input[index1] = input[index2];
+        input[index2] = tmp;
+    }
 
     /**
      * Splits the range into two partitions.
@@ -20,7 +46,7 @@ public abstract class PartitioningStrategy<T extends Comparable<T>> {
      * @return Partition index.
      */
     public int execute(final T[] input, final int left, final int right) {
-        return this.partition(input, left, right, this.findPivotIndex(input, left, right));
+        return PartitioningStrategy.partition(input, left, right, this.findPivotIndex(input, left, right));
     }
 
     /**
@@ -35,26 +61,5 @@ public abstract class PartitioningStrategy<T extends Comparable<T>> {
      * @return Pivot index.
      */
     protected abstract int findPivotIndex(final T[] input, final int left, final int right);
-
-    private int partition(final T[] input, final int left, final int right, final int pivotIndex) {
-        final T pivotValue = input[pivotIndex];
-        this.swap(input, pivotIndex, right);
-        int storeIndex = left;
-        for (int i = left; i < right; i++) {
-            if (input[i].compareTo(pivotValue) >= 0) {
-                continue;
-            }
-            this.swap(input, i, storeIndex);
-            storeIndex++;
-        }
-        this.swap(input, storeIndex, right);
-        return storeIndex;
-    }
-
-    private void swap(final T[] input, final int index1, final int index2) {
-        final T tmp = input[index1];
-        input[index1] = input[index2];
-        input[index2] = tmp;
-    }
 
 }
